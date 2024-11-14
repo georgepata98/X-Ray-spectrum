799e9134a7c32995dc32a16de328cdd20984069e
George <122010570+georgepata98@users.noreply.github.com>
2024-11-14 18:13:40 +0200

    Add files via upload

---
+++ xr_spectrum.C
@@ -0,0 +1,38 @@
+{
+    TCanvas *c1 = new TCanvas("c1", "MyCanvas", 800, 600);
+    TH1F *h1 = new TH1F("h1", "histogram", 150, 0, 150);
+
+    fstream file;
+    string fileName;
+    float energy;
+    int x=0;
+
+
+    cout << "---------- Program care face spectrul razelor X ----------" << endl << endl;
+
+    cout << "Introduceti numele fisierului .txt cu valorile energiilor depozitate de razele X:" << endl;
+    cin >> fileName;
+
+    file.open(fileName + ".txt", ios::in);
+
+    if(!file)
+    {
+        cout << "EROARE: Nu a fost gasit fisierul " << fileName << " !" << endl;
+        return 1;
+    }
+
+    while(!file.eof())
+    {
+        file >> energy;
+        h1->Fill(x, energy);
+        x++;
+    }
+
+    file.close();
+
+
+    h1->SetTitle("Geant4 X-rays spectrum for 150 kVp, 0.8 mm Be");
+    h1->GetXaxis()->SetTitle("Energy [keV]");
+    h1->GetYaxis()->SetTitle("Counts");
+    h1->Draw();
+}
